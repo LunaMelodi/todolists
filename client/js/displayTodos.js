a65f3fbffd2b9384b5d40b6d2d341ac4874768ac
Joel Pereira <joel.pereira.996@gmail.com>
2020-10-20 14:17:01 -0300

    checkbox and remove button(functionless) added

--- client/js/displayTodos.js
+++ client/js/displayTodos.js
@@ -3,7 +3,7 @@ export default async function displayTodos(todos) {
 
   ul.innerHTML='';
 
-  todos.forEach(el => {
-    ul.innerHTML += `<li class='todo-item'>${el['todo-content']}</li>`;
+  todos.forEach((el, i)=> {
+    ul.innerHTML += `<li class='todo-item'><input type="checkbox" id="todo${i}" name="todo" value="Todo">${el['todo-content']}<button>x</button></li>`;
   });
 }
