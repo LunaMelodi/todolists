0d22cb58caae0527f668e4b264df7c3f275655b3
Joel Pereira <joel.pereira.996@gmail.com>
2020-10-20 15:27:08 -0300

    list items positioned

--- client/js/displayTodos.js
+++ client/js/displayTodos.js
@@ -4,6 +4,6 @@ export default async function displayTodos(todos) {
   ul.innerHTML='';
 
   todos.forEach((el, i)=> {
-    ul.innerHTML += `<li class='todo-item'><input type="checkbox" id="todo${i}" name="todo" value="Todo">${el['todo-content']}<button>x</button></li>`;
+    ul.innerHTML += `<li class='todo-item'><input type="checkbox" id="todo${i}" name="todo" value="Todo"><p class="list-text">${el['todo-content']}</p><button>x</button></li>`;
   });
 }
