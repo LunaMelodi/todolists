3e66ccd6c01e5d3433cda06d1564227946d602fa
Luna <59760696+LunaMelodi@users.noreply.github.com>
2020-10-23 17:38:07 -0400

    Finished user auth signup and login route. Is working.

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,19 +1,37 @@
 import UserService from '../services/UserService.js';
 import ResGen from '../utils/ResGeneration.js';
+import * as argon2 from 'argon2';
+import { randomBytes } from 'crypto';
 
 const resgen = new ResGen();
 
 class AuthController {
   static async signup(req, res) {
     if (validateUser(req.body)) {
-      let theUser = await UserService.getOneUserByEmail(req.body.email);
+      let userRecord = await UserService.getOneUserByEmail(req.body.email);
 
-      if (!theUser) {
+      if (!userRecord) {
+        const salt = randomBytes(32);
+        const passwordHashed = await argon2.hash(req.body.password, { salt });
+        
+        UserService.addUser({
+          name: req.body.name,
+          email: req.body.email,
+          password: passwordHashed
+        })
         res.json({
+          user: {
+            name: req.body.name,
+            email: req.body.email,
+          }
+        })
+        
+        
+        /* res.json({
           message: '✅'
-        });
+        }); */
       } else {
-        console.log('theUser :>> ', theUser);
+        console.log('userRecord :>> ', userRecord);
 
         resgen.setError(400, 'Email is already in use.');
         return resgen.send(res);
@@ -25,9 +43,32 @@ class AuthController {
     }
   }
   static async login(req, res) {
-    res.json({
-      message: 'hi'
-    })
+    if (validateUser(req.body)) {
+      const userRecord = await UserService.getOneUserByEmail(req.body.email);
+      
+      if(userRecord) {
+        console.log('userRecord :>> ', userRecord);
+
+        const correctPassword = await argon2.verify(userRecord.password, req.body.password);
+        
+        if (correctPassword) {
+          res.json({
+            message: 'success. logging in...',
+            user: userRecord
+          })
+        } else {
+            resgen.setError(400, 'Incorrect password');
+            return resgen.send(res);
+        }      
+      } else {
+        resgen.setError(400, 'User not found in database');
+        return resgen.send(res);
+      }
+      
+    } else {
+      resgen.setError(400, 'Invalid user');
+      return resgen.send(res);
+    }
   }
   static async logout(req, res) {
     res.json({
