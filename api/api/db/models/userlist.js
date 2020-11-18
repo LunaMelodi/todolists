cf6a813528f30cfe2b7535e372d2068650893dad
Joel Pereira <joel.pereira.996@gmail.com>
2020-11-17 21:37:35 -0300

    patching API folder

---
+++ api/api/db/models/userlist.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserList extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  UserList.init({
+    userId: DataTypes.UUID,
+    listId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserList',
+  });
+  return UserList;
+};
+
+module.exports = (sequelize, DataTypes) => {
+  const UserList = sequelize.define('UserLists', {
+    userId: DataTypes.UUID,
+    listId: DataTypes.INTEGER
+  });
+
+  UserList.associate = function(models) {
+    UserList.belongsTo(models.Lists, {
+      foreignKey: 'listId', 
+    }),
+    UserList.belongsTo(models.Users, {
+      foreignKey: 'userId', 
+    })
+  }
+
+  return UserList;
+}
